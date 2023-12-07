3a4b1a10dfe8a02b0a5a7cd4c7417333e2a16541
aliemrepmk <aliemrepamuk0@gmail.com>
2023-12-07 20:10:18 +0300

    changed subscript operator to functions

--- src/levenshtein.cpp
+++ src/levenshtein.cpp
@@ -12,20 +12,20 @@ int levenshtein::calculate_distance(std::string_view source,
   std::vector<int> current_row(target_length + 1, 0);
 
   for (int j = 0; j <= target_length; j++) {
-    previous_row[j] = j;
+    previous_row.at(j) = j;
   }
 
   for (int i = 1; i <= source_length; i++) {
-    current_row[0] = i;
+    current_row.at(0) = i;
 
     for (int j = 1; j <= target_length; j++) {
-      if (source[i - 1] == target[j - 1]) {
-        current_row[j] = previous_row[j - 1];
+      if (source.at(i - 1) == target.at(j - 1)) {
+        current_row.at(j) = previous_row.at(j - 1);
         continue;
       }
 
-      int cost = std::min(previous_row[j], previous_row[j - 1]);
-      current_row[j] = 1 + std::min(current_row[j - 1], cost);
+      int cost = std::min(previous_row.at(j), previous_row.at(j - 1));
+      current_row.at(j) = 1 + std::min(current_row.at(j - 1), cost);
     }
 
     previous_row = current_row;
